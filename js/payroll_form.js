63224f94b32fb3f1c8e7516a070a8d945f8941b0
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-30 04:03:31 +0530

    Update payroll_form.js

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -66,5 +66,71 @@ class EmployeePayRoll
       throw 'Start Date is beyond 30 Days!';
     this._startDate = startDate; 
   }
-   
+   var salary = document.querySelector('#salary');
+var output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+});
+
+var save = () =>{
+    try{
+        let employeePayrollData = createEmployeePayroll();
+    }catch(e){
+        return;
+    }
+}
+
+var createEmployeePayroll = () =>{
+    let employeePayrollData = new EmployeePayrollData();
+    try{
+        employeePayrollData.name = getInputValueById('#name');
+    }catch(e){
+        setTextValue('.text-error',e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+    employeePayrollData.startDate = new Date(Date.parse(date));
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+var getSelectedValues = (propertyValue) =>{
+    let allItems = document.querySelectorAll(propertyValue);
+    let selectedItems = [];
+    allItems.forEach(item=>{
+        if(item.checked){
+            selectedItems.push(item.value);
+        }
+    });
+    return selectedItems;
+}
+
+/*
+* 1: querySelector is the newer feature.
+* 2: The querySelector method can be used when selecting by element name,
+*    nesting, or class name.
+* 3: querySelector lets you find elements with rules that can't be 
+*    expressed with getElementById
+*/
+var getInputValueById = (id) =>{
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+/*
+* 1: getElementById is better supported than querySelector in older 
+*    versions of the browsers.
+* 2: The thing with getElementById is that it only allows to select an 
+*    element by its id.
+*/
+var getInputElementValue = (id) =>{
+    let value = document.getElementById(id).value;
+    return value;
+}
 }
